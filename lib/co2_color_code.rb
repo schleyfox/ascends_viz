a6b76decde0ab2a790fe4a53f7e2316d4b713cd9
Ben Hughes <ben@pixelmachine.org>
2008-06-24 16:10:22 -0400

    Added task to create color bar for CO2
    * only generates PNG, KML is manual for now

--- lib/co2_color_code.rb
+++ lib/co2_color_code.rb
@@ -1,14 +1,27 @@
+require 'lib/ruby-processing'
+
+$co2_low_bound = 370
+$co2_high_bound = 450
+
 class Co2ColorCode
   def self.colorify(value)
     v = value.to_f
     if v == -9999.99
       return abgr(0,0,0,0)
     else
-      v = normalize(v, 370, 450)
-      c = hsv(v*360, 0.75, 0.75)
-      return abgr(255 ,255*c[2], 255*c[1], 255*c[0])
+      c = normalized_colorify(normalize(v, $co2_low_bound, $co2_high_bound))
+      abgr(255 ,255*c[2], 255*c[1], 255*c[0])
     end
   end
+
+  def self.normalized_colorify(norm_value)
+    hsv((norm_value*300) +60, 0.75, 0.75)
+  end
+
+  def self.make_color_bar
+    Co2ColorCodeBar.new :title => "Color Bar", :height => 400, :width => 60 
+  end
+
 
   def self.abgr(a,b,g,r)
     "%02X%02X%02X%02X" % [a,b,g,r]
@@ -46,6 +59,37 @@ class Co2ColorCode
     else
       return (value-min)/(max-min)
     end
+  end
+
+  def self.lerp(min, max, value)
+    (max-min)*value + min
+  end
+end
+
+class Co2ColorCodeBar < Processing::App
+  def setup
+    background(255)
+    font = load_font("#{GTRON_ROOT}/lib/Electron-12.vlw")
+    text_font(font)
+    fill(0)
+    last_line_number = nil
+    for i in (0...height)
+      value = (1.0/height)*i
+      colors = Co2ColorCode.normalized_colorify(value).map{|v| (255*v).to_i } 
+      stroke(colors[0], colors[1], colors[2])
+      line(25,i,width,i)
+
+      orig_val = Co2ColorCode.lerp($co2_low_bound, $co2_high_bound, value).to_i
+      
+      if (orig_val % 10 == 0) && orig_val != last_line_number
+        last_line_number = orig_val
+        text("#{orig_val}", 2, i+12)
+        #stroke(0)
+        #line(23,i,height,i)
+      end
+    end
+    save "#{GTRON_ROOT}/output/co2_color_bar.png"
+    exit
   end
 end
 
---
+++ tasks/make_color_bar.rake
@@ -0,0 +1,7 @@
+desc "Creates Color Bar for Google Earth"
+task :make_color_bar do
+  Co2ColorCode.make_color_bar
+#  kml = KMLFile.new
+#  overlay = KML::ScreenOverlay.new(:name => 'CO2 Color Bar',
+#                                   overlay
+end
